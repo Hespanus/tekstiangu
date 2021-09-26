1c27460303f20f646a9e619b6af3daa169a7f867
HeikkiTanhi <heikki@tanhi-systems.com>
2021-09-27 00:32:58 +0300

    Service ja palvelinasetusten pitäisi olla kunnossa

--- src/app/peli.service.ts
+++ src/app/peli.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import {HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import {Kysymys} from "./kysymys";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliService {
+  private apiUrl = 'http://localhost/peli.json';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+  getKysymys(): Observable<Kysymys[]> {
+    return this.http.get<Kysymys[]>(this.apiUrl);
+  }
 
-  getKysymys(){}
+
 }
--- src/app/pelialue/pelialue.component.ts
+++ src/app/pelialue/pelialue.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Kysymys} from '../kysymys';
+import {PeliService} from "../peli.service";
 
 @Component({
   selector: 'app-pelialue',
@@ -6,12 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pelialue.component.css']
 })
 export class PelialueComponent implements OnInit {
+  kysymykset: Kysymys[] = [];
 
-  constructor() { }
+  kysind = [1,2,3,4];
+  pisteetmax = 0;
+  vastpisteet = 0;
+  pisteetyht = 0;
+  id = 1;
+  palaute = " ";
+
+  constructor(private peliService: PeliService) { }
 
   ngOnInit(): void {
+    this.peliService.getKysymys().subscribe((data) => (this.kysymykset = data));
   }
   valinta(){
-    console.log('Pupu');
+
   }
 }

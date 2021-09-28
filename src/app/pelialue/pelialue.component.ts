a331cb4e2d0b7be6167195ba200530456476b84d
heikki <heikki@tanhi-systems.com>
2021-09-28 15:59:11 +0300

    valmis

--- src/app/pelialue/pelialue.component.ts
+++ src/app/pelialue/pelialue.component.ts
@@ -11,7 +11,7 @@ export class PelialueComponent implements OnInit {
   kysymykset!: Kysymys[];
 
   kysind: number[] = [1,2,3,4];
-  pisteetmax: number = 0;
+  pisteetmax: number | undefined = 0;
   vastpisteet: number = 0;
   pisteetyht: number = 0;
   id: number = this.kysind[Math.floor(Math.random() * this.kysind.length)];
@@ -31,9 +31,55 @@ export class PelialueComponent implements OnInit {
     }
     return 0;
   }
-
+  arvonta() {
+    this.id = this.kysind[Math.floor(Math.random() * this.kysind.length)];
+  }
   valinta(vastaus: object){
+    for (let i = 0; i < this.kysind.length; i++) {
+      if (this.kysind[i] === this.id) {
+        this.kysind.splice(i, 1);
+      }
+    }
+    // @ts-ignore
+    this.palaute = vastaus.info;
+    this.pisteetmax = 0;
+    // @ts-ignore
+    this.vastpisteet = vastaus.pisteet;
+    this.pisteetyht += this.vastpisteet;
+
+    // @ts-ignore
+    for (let vast of (this.kysymykset)[this.haku()].Vastaukset){
+
+      // @ts-ignore
+      if(vast.pisteet > this.pisteetmax){
+        this.pisteetmax = vast.pisteet;
+      }
+    }
+
+    // @ts-ignore
+    if(this.id === 999 && vastaus.kysymys === 1){
+      this.pisteetyht = 0;
+      this.kysind = [1, 2, 3, 4];
+      this.arvonta();
+    }
+    // @ts-ignore
+    if(vastaus.kysymys === 998 && this.kysind.length === 0){
+      this.id = 999;
+
+    } else { // @ts-ignore
+      if(vastaus.kysymys === 998){
+            this.arvonta();
+          } else {
+            // @ts-ignore
+        this.id = vastaus.kysymys;
+
+          }
+    }
+
+
 
 
   }
+
+
 }

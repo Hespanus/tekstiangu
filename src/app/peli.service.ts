99e0578d3bddc78938f00bf8b11605c710df51fd
heikki <heikki@tanhi-systems.com>
2021-10-20 15:42:16 +0300

    get service

--- src/app/peli.service.ts
+++ src/app/peli.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import {Kysymys} from "./kysymys";
 import {Artikkeli} from "./artikkeli";
 import {Pelaaja} from "./pelaaja";
+import {tsCastToAny} from "@angular/compiler-cli/src/ngtsc/typecheck/src/ts_util";
+
+/*const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    Authorization: 'my-auth-token'
+  })
+};*/
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliService {
   private apiUrl = 'http://localhost/';
+
+  private playerUrl = 'http://localhost/addplayer';
 
 
 
@@ -25,5 +35,22 @@ export class PeliService {
   getPelaaja(file: string): Observable<Pelaaja[]> {
     return this.http.get<Pelaaja[]>(this.apiUrl + file);
   }
+  addPlayer(): Observable<any>{
+    return this.http.post<any>(this.playerUrl, )
 
+  }
+
+ /* private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+
+      console.error('An error occurred:', error.error);
+    } else {
+
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+
+    return throwError(
+      'Something bad happened; please try again later.');
+  }*/
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {PeliService} from "../peli.service";
 
 @Component({
   selector: 'app-signup',
@@ -9,9 +12,12 @@ export class SignupComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(private peliService: PeliService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(f: NgForm) {
+
+  }
 }
